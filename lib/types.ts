6dc26e5863ed82bd869d9a37aee41ddedd6f15ae
JayGuri <165677478+JayGuri@users.noreply.github.com>
2025-01-08 00:28:34 +0530

    Modified types.ts for deployment

--- lib/types.ts
+++ lib/types.ts
@@ -1,15 +1,17 @@
 export interface Course {
-    name: string;
-    instructor: string;
-    rating: number;
-    techStack: string[];
-    overview: string;
-    detailedContent: string;
-    duration: string;
-    tags: string[];
-    image: string;
-    reviews: string[];
-    price: number; 
-  }
-  
-  
+  name: string;
+  instructor: string;
+  instructor_photo: string;
+  instructor_description: string;
+  rating: number;
+  techStack: string[];
+  overview: string;
+  detailedContent: string;
+  duration: string;
+  tags: string[];
+  image: string;
+  dummyvideo_link: string;
+  reviews: string[];
+  price: number;
+}
+
